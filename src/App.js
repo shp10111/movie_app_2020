ccc074e5e9df4a8db3f5f440ad5b41f3e4195f88
shp10111 <shp10111@gmail.com>
2020-09-18 16:19:56 +0900

    Edit readme.md

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
 
+function Food(props){
+  console.log(props)
+  return(
+    <h3>I love potato {props.fav}</h3>
+  );
+}
+
+
 function App() {
-  return <div className="App" />;
+  return (
+  <div>
+    <h1> hello </h1>
+    <Food fav="김치" />
+    <Food fav="치킨" />
+    <Food fav="계란" />
+    <Food fav="피자" />
+    <Food fav="콜라" />
+    </div>
+  );
 
 }
 
