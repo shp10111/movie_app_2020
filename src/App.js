c1419c4f2ffd157e07331a9c943e7c5e01772d89
shp10111 <shp10111@gmail.com>
2020-10-07 14:11:47 +0900

    1007

--- src/App.js
+++ src/App.js
@@ -7,17 +7,22 @@ function Food(props){
     <h3>I love potato {props.fav}</h3>
   );
 }
-
+const foodLink = [
+  {
+    name : '김치',
+  },
+  {
+    name : '피자',
+  },
+  {
+    name : '콜라',
+  }
+]
 
 function App() {
   return (
   <div>
     <h1> hello </h1>
-    <Food fav="김치" />
-    <Food fav="치킨" />
-    <Food fav="계란" />
-    <Food fav="피자" />
-    <Food fav="콜라" />
     </div>
   );
 
