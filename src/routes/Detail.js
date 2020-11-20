c9bea75a025c5579f1c2513e74fe1e8d838444fd
shp10111 <shp10111@gmail.com>
2020-11-20 16:57:14 +0900

    1120업로드

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Detail.css';
 
 class Detail extends React.Component{
+    
     componentDidMount(){
         const {location, history} = this.props;
 
@@ -11,13 +12,18 @@ class Detail extends React.Component{
     }
     render(){
         const {location} = this.props;
+        
         if(location.state){
+            function star1(){
+                var num = location.state.rating;
+                return document.write("☆" * num);
+            }
             return (
                 <div className="movie">
                 <img src={location.state.poster} alt={location.state.title}></img>
                     <div className="movie-data">
                         <h1 className="movie-title">{location.state.title}</h1>
-                        <h3 className="movie-year">{location.state.year} / {location.state.rating} {location.state.rating}</h3>
+                        <h3 className="movie-year">{location.state.year} / {location.state.rating}{star1}</h3>
                         <h3 className="movie-summary">{location.state.summary}</h3>
                         <h3 className="movie-genres">{location.state.genres}</h3>
                     </div>
@@ -29,5 +35,5 @@ class Detail extends React.Component{
     }
 }
 
-
+//{"☆"*(parseInt)(location.state.rating/2+1)}
 export default Detail;
